49c093430957d907aa3a9793a36f337bddc9cf17
Manuel <ruizsantiago@gmail.com>
2017-09-23 09:52:57 +0200

    Adding comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 class Comments extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault()
+    const { postId } = this.props.params
+    const author = this.refs.author.value
+    const comment = this.refs.comment.value
+    this.props.addComment(postId, author, comment)
+  }
+
   renderComment(comment, i) {
     return(
       <div className="comment" key={i}>
@@ -17,7 +25,7 @@ class Comments extends React.Component {
     return(
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="comment-form" className="comment-form">
+        <form ref="comment-form" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
